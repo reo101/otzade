20222248e88cdd0d7383d8588ee4f5a912f4f030
reo101 <pavel.atanasov2001@gmail.com>
2026-06-10 00:56:53 +0300

    feat(flake): add

---
+++ flake.nix
@@ -0,0 +1,31 @@
+# DO-NOT-EDIT. This file was auto-generated using github:vic/flake-file.
+# Use `nix run .#write-flake` to regenerate it.
+{
+  outputs = inputs: import ./outputs.nix inputs;
+
+  nixConfig = {
+    commit-lockfile-summary = "chore(flake): update `flake.lock`";
+    extra-experimental-features = [ "pipe-operators" ];
+  };
+
+  inputs = {
+    flake-file.url = "github:vic/flake-file";
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    systems.url = "github:nix-systems/default";
+    zig-flake = {
+      url = "github:silversquirl/zig-flake";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    zls = {
+      url = "github:zigtools/zls/0.16.0";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        zig-flake.follows = "zig-flake";
+      };
+    };
+  };
+}
